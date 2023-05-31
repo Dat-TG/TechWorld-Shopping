018607bd8fbe9ccea9ed12114efb29c2337673e8
20120454 <dat13102k2@gmail.com>
2023-05-31 23:09:48 +0700

    add loading animation

--- app/components/form/Login.tsx
+++ app/components/form/Login.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
 import { signIn, useSession } from 'next-auth/react';
-import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { Notify } from 'notiflix';
 
 export default function Login() {
     const session = useSession();
@@ -13,6 +13,7 @@ export default function Login() {
     function togglePasswordVisibility() {
         setIsPasswordVisible(prevState => !prevState);
     }
+    const [logining, setLogining] = useState(false);
 
     useEffect(() => {
         if (session?.status === 'authenticated') {
@@ -44,11 +45,13 @@ export default function Login() {
             redirect: false,
         }).then(callback => {
             if (callback?.error) {
-                toast.error('Đăng nhập thất bại');
+                Notify.failure(
+                    'Đăng nhập không thành công. Số điện thoại hoặc mật khẩu không chính xác',
+                );
             }
 
             if (callback?.ok && !callback?.error) {
-                toast.success('Đăng nhập thành công');
+                Notify.success('Đăng nhập thành công');
                 router.push('/');
             }
         });
@@ -156,9 +159,37 @@ export default function Login() {
                             <div>
                                 <button
                                     type='submit'
-                                    className='flex w-full justify-center rounded-md bg-amber-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-amber-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300'
+                                    className={
+                                        'relative flex w-full justify-center rounded-md bg-amber-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-amber-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300 ' +
+                                        (logining ? 'opacity-20' : '')
+                                    }
+                                    onClick={() => setLogining(true)}
                                 >
                                     Đăng nhập
+                                    {logining && (
+                                        <div
+                                            role='status'
+                                            className='absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 opacity-100'
+                                        >
+                                            <svg
+                                                aria-hidden='true'
+                                                className='w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+                                                viewBox='0 0 100 101'
+                                                fill='none'
+                                                xmlns='http://www.w3.org/2000/svg'
+                                            >
+                                                <path
+                                                    d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                                                    fill='currentColor'
+                                                />
+                                                <path
+                                                    d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                                                    fill='currentFill'
+                                                />
+                                            </svg>
+                                            <span className='sr-only'>Loading...</span>
+                                        </div>
+                                    )}
                                 </button>
                             </div>
                         </form>
