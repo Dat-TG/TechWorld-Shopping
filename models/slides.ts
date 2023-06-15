1901840c7725c3c229688d517cb58a8e8467748b
20120454 <dat13102k2@gmail.com>
2023-06-15 14:15:24 +0700

    add / before img src

--- models/slides.ts
+++ models/slides.ts
@@ -1,164 +1,164 @@
 export const mainCarousel = [
     {
-        url: 'https://lh3.googleusercontent.com/vkMMX2cvl_1ii0c_vw5TGy4ixhRc-l7OlMWnmx4-oxquqHo_A9aET_lWxDmxbh-GMZTr3O5JS4kGNa0Ka7hcctxo2lj0xoUR=w1920-rw',
+        url: 'https:/lh3.googleusercontent.com/vkMMX2cvl_1ii0c_vw5TGy4ixhRc-l7OlMWnmx4-oxquqHo_A9aET_lWxDmxbh-GMZTr3O5JS4kGNa0Ka7hcctxo2lj0xoUR=w1920-rw',
         href: '#',
     },
     {
-        url: 'https://lh3.googleusercontent.com/NEyGqAS4HkBmVGWbdLxRCJ7v4n7Xz-Xcfs6ffoxCNZMHBg0txwJk7L0FVyBvjZ9mwdFsV915-uAWlcX_JPHD1yJSq2EYfeV6=w1920-rw',
+        url: 'https:/lh3.googleusercontent.com/NEyGqAS4HkBmVGWbdLxRCJ7v4n7Xz-Xcfs6ffoxCNZMHBg0txwJk7L0FVyBvjZ9mwdFsV915-uAWlcX_JPHD1yJSq2EYfeV6=w1920-rw',
         href: '#',
     },
     {
-        url: 'https://lh3.googleusercontent.com/38VYfGRFwWmHsd9kSzW3Dp86ir87GrQ2QYO5QrnUMNQNKcq8wriHChwsim9NSZaAg-eNAPPHl-lQq8LxeAV9PZUzAPiI1udK=w1920-rw',
+        url: 'https:/lh3.googleusercontent.com/38VYfGRFwWmHsd9kSzW3Dp86ir87GrQ2QYO5QrnUMNQNKcq8wriHChwsim9NSZaAg-eNAPPHl-lQq8LxeAV9PZUzAPiI1udK=w1920-rw',
         href: '#',
     },
     {
-        url: 'https://lh3.googleusercontent.com/FeE-2OLVlgr0rLqJcHXyzuvEEpgnYp1XBJ60hZq6YD4sZgHlzYJMFcAS66_79y-rBWOFhvamh5zKy8jjIuHuhhuIOJ2pA_Q=w1920-rw',
+        url: 'https:/lh3.googleusercontent.com/FeE-2OLVlgr0rLqJcHXyzuvEEpgnYp1XBJ60hZq6YD4sZgHlzYJMFcAS66_79y-rBWOFhvamh5zKy8jjIuHuhhuIOJ2pA_Q=w1920-rw',
         href: '#',
     },
     {
-        url: 'https://cdn.shopify.com/s/files/1/0298/6259/2571/files/SSO_-_CH520_1920x613_420e810a-4e6f-4a30-9f97-d2855c129f43_1600x800.progressive.jpg?v=1684131800',
+        url: 'https:/cdn.shopify.com/s/files/1/0298/6259/2571/files/SSO_-_CH520_1920x613_420e810a-4e6f-4a30-9f97-d2855c129f43_1600x800.progressive.jpg?v=1684131800',
         href: '#',
     },
     {
-        url: 'https://cdn11.dienmaycholon.vn/filewebdmclnew/DMCL21/Picture//Tm/Tm_picture_343/tu-lanh-diet-kh_388_1200.png.webp',
+        url: 'https:/cdn11.dienmaycholon.vn/filewebdmclnew/DMCL21/Picture/Tm/Tm_picture_343/tu-lanh-diet-kh_388_1200.png.webp',
         href: '#',
     },
 ];
 
 export const promotion = [
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/05/banner/vivo-V27e-720-220-720x220-2.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/05/banner/vivo-V27e-720-220-720x220-2.png',
         href: '#',
     },
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/05/banner/720-220-720x220-54.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/05/banner/720-220-720x220-54.png',
         href: '#',
     },
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/05/banner/a57-720-220-720x220-2.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/05/banner/a57-720-220-720x220-2.png',
         href: '#',
     },
 
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/04/banner/720-220-720x220-67.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/04/banner/720-220-720x220-67.png',
         href: '#',
     },
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/04/banner/DH-XA-HANG-720-220-720x220.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/04/banner/DH-XA-HANG-720-220-720x220.png',
         href: '#',
     },
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/04/banner/720-220-720x220-83.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/04/banner/720-220-720x220-83.png',
         href: '#',
     },
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/04/banner/iP-14-720-220-720x220-1.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/04/banner/iP-14-720-220-720x220-1.png',
         href: '#',
     },
     {
-        url: 'https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/04/banner/realme-C55-720-220-720x220-2.png',
+        url: 'https:/img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https:/cdn.tgdd.vn/2023/04/banner/realme-C55-720-220-720x220-2.png',
         href: '#',
     },
 ];
 
 export const listCat = [
     {
-        img: 'images/cat/Laptop-129x129.webp',
+        img: '/images/cat/Laptop-129x129.webp',
         name: 'Laptop',
         href: '/category/laptop',
     },
     {
-        img: 'images/cat/Tablet-128x129.webp',
+        img: '/images/cat/Tablet-128x129.webp',
         name: 'Tablet',
         href: '/category/tablet',
     },
     {
-        img: 'images/cat/Donghothongminh-128x129.webp',
+        img: '/images/cat/Donghothongminh-128x129.webp',
         name: 'Đồng hồ thông minh',
         href: '/category/smartwatch',
     },
     {
-        img: 'images/cat/dienthoai.jpg',
+        img: '/images/cat/dienthoai.jpg',
         name: 'Điện thoại',
         href: '/category/dien-thoai',
     },
     {
-        img: 'images/cat/Donghothoitrang-128x129.webp',
+        img: '/images/cat/Donghothoitrang-128x129.webp',
         name: 'Đồng hồ thời trang',
         href: '/category/dong-ho',
     },
     {
-        img: 'images/cat/maycu.jpg',
+        img: '/images/cat/maycu.jpg',
         name: 'Máy cũ giá rẻ',
         href: '/category/may-cu-gia-re',
     },
     {
-        img: 'images/cat/Oplung-128x128.webp',
+        img: '/images/cat/Oplung-128x128.webp',
         name: 'Ốp lưng  điện thoại',
         href: '/category/op-lung-dien-thoai',
     },
     {
-        img: 'images/cat/chuot-128x129.webp',
+        img: '/images/cat/chuot-128x129.webp',
         name: 'Chuột máy tính',
         href: '/category/chuot',
     },
     {
-        img: 'images/cat/ban-phim-128x129.webp',
+        img: '/images/cat/ban-phim-128x129.webp',
         name: 'Bàn phím',
         href: '/category/ban-phim',
     },
     {
-        img: 'images/cat/mousepad.jpg',
+        img: '/images/cat/mousepad.jpg',
         name: 'Lót chuột',
         href: '/category/lot-chuot',
     },
     {
-        img: 'images/cat/Loa-128x128.webp',
+        img: '/images/cat/Loa-128x128.webp',
         name: 'Loa',
         href: '/category/loa',
     },
     {
-        img: 'images/cat/Tainghe-128x129.webp',
+        img: '/images/cat/Tainghe-128x129.webp',
         name: 'Tai nghe',
         href: '/category/tai-nghe',
     },
     {
-        img: 'images/cat/Sacduphong-128x129.webp',
+        img: '/images/cat/Sacduphong-128x129.webp',
         name: 'Sạc dự phòng',
         href: '/category/pin-sac-du-phong',
     },
     {
-        img: 'images/cat/Capsac-129x129.webp',
+        img: '/images/cat/Capsac-129x129.webp',
         name: 'Cáp sạc',
         href: '/category/cap-sac',
     },
     {
-        img: 'images/cat/Maytinhbo-128x129.webp',
+        img: '/images/cat/Maytinhbo-128x129.webp',
         name: 'Máy tính bộ',
         href: '/category/pc',
     },
     {
-        img: 'images/cat/Mayin-128x129.webp',
+        img: '/images/cat/Mayin-128x129.webp',
         name: 'Máy in',
         href: '/category/may-in',
     },
     {
-        img: 'images/cat/Manhinhmaytinh-128x129.webp',
+        img: '/images/cat/Manhinhmaytinh-128x129.webp',
         name: 'Màn hình',
         href: '/category/man-hinh',
     },
     {
-        img: 'images/cat/Phukiengaming-128x129.webp',
+        img: '/images/cat/Phukiengaming-128x129.webp',
         name: 'Phụ kiện gaming',
         href: '/category/phu-kien-gaming',
     },
     {
-        img: 'images/cat/icon-camera-128x129.webp',
+        img: '/images/cat/icon-camera-128x129.webp',
         name: 'Camera, webcam',
         href: '/category/camera-webcam',
     },
     {
-        img: 'images/cat/may-chieu-icon-128x129.webp',
+        img: '/images/cat/may-chieu-icon-128x129.webp',
         name: 'Ổ cắm, bòng đèn thông minh',
         href: '/category/o-cam-bong-den-thong-minh',
     },
