2b3fe3b4cb5a37efa0997a2c14b121b3fe44d462
20120454 <dat13102k2@gmail.com>
2023-06-19 16:53:06 +0700

    admin/load list users

--- app/(admin)/admin/user/[page]/page.tsx
+++ app/(admin)/admin/user/[page]/page.tsx
@@ -6,12 +6,12 @@ export const metadata = {
     icons: '/images/logo.png',
 };
 
-export default async function Page() {
+export default async function Page({ params }: { params: { page: string } }) {
     const perPage = 10;
     const totalUser = await numberOfUsers();
     return (
         <div className='w-full'>
-            <UserList perPage={perPage} totalUsers={totalUser} page={1} />
+            <UserList perPage={perPage} totalUsers={totalUser} page={parseInt(params.page)} />
         </div>
     );
 }
